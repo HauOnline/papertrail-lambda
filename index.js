49963583782ca1b85f8e83670e7a66115ad89669
Matheus Peviani Vellone <matheusvellone@gmail.com>
2021-05-02 16:33:22 -0300

    first version

---
+++ index.js
@@ -0,0 +1,72 @@
+const zlib = require('zlib')
+const winston = require('winston')
+require('winston-papertrail')
+
+const unarchiveLogData = async (payload) => {
+  const rawData = await new Promise((resolve, reject) => {
+    zlib.gunzip(payload, (err, result) => {
+      if (err) {
+        return reject(err)
+      }
+
+      return resolve(result)
+    })
+  })
+  
+  return JSON.parse(rawData.toString('utf8'))
+}
+
+function getEnvVarOrFail(varName) {
+  const value = process.env[varName]
+  if (!value) {
+    throw new Error(`Required environment variable ${varName} is undefined`)
+  }
+  return value
+}
+
+// Should match winston simple log format for example: "error: The database has exploded"
+// For more information see https://github.com/winstonjs/winston
+// The pattern represents the following:
+// A sequence of non-tab chars at the start of input followed by a tab
+// Another sequence of non-tabs followed by a tab
+// Capture a group of alphanumeric chars leading up to a ':'
+const logLevelRegex = /^[^\t]+\t[^\t]+\t(\w+):/
+function parseLogLevel(tsvMessage) {
+  // Messages logged manually are tab separated value strings of three columns:
+  // date string (ISO8601), request ID, log message
+  const match = logLevelRegex.exec(tsvMessage)
+  return match && match[1].toLowerCase()
+}
+
+exports.handler = async (event, context, callback) => {
+  try {
+    const host = getEnvVarOrFail('PAPERTRAIL_HOST')
+    const port = getEnvVarOrFail('PAPERTRAIL_PORT')
+
+    const payload = Buffer.from(event.awslogs.data, 'base64')
+
+    const logData = await unarchiveLogData(payload)
+      
+    const papertrailTransport = new winston.transports.Papertrail({
+      host,
+      port,
+      program: logData.logStream,
+      hostname: logData.logGroup,
+      flushOnClose: true,
+      colorize: true,
+    })
+    const logger = new (winston.Logger)({
+      transports: [papertrailTransport],
+    })
+
+    logData.logEvents.forEach((event) => {
+      const logLevel = parseLogLevel(event.message) || 'info'
+
+      logger.log(logLevel, event.message)
+    })
+    logger.close()
+    
+  } catch (error) {
+    callback(error)
+  }
+}
